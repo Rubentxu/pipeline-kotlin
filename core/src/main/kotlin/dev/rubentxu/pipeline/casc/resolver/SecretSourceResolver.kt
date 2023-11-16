35392e1e22f95e6bee71143e0fe1549f495ca112
Rubentxu <e_rcabre@mercadona.com>
2023-11-16 16:02:37 +0100

    secret resolver

--- core/src/main/kotlin/dev/rubentxu/pipeline/casc/resolver/SecretSourceResolver.kt
+++ core/src/main/kotlin/dev/rubentxu/pipeline/casc/resolver/SecretSourceResolver.kt
@@ -1,16 +1,14 @@
-package dev.rubentxu.pipeline.casc
+package dev.rubentxu.pipeline.casc.resolver
 
 
 import dev.rubentxu.pipeline.logger.PipelineLogger
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+//import kotlinx.serialization.json.Json
+//import kotlinx.serialization.json.jsonObject
+//import kotlinx.serialization.json.jsonPrimitive
 import org.apache.commons.lang3.StringUtils
 import org.apache.commons.text.StringSubstitutor
 import org.apache.commons.text.TextStringBuilder
 import org.apache.commons.text.lookup.StringLookup
-import org.eclipse.jgit.annotations.NonNull
-import org.gradle.internal.impldep.javax.annotation.CheckForNull
 import java.io.IOException
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
@@ -18,10 +16,12 @@ import java.nio.file.InvalidPathException
 import java.nio.file.Paths
 import java.util.*
 
+
+
 /**
  * Resolves secret variables and converts escaped internal variables.
  */
-class SecretSourceResolver {
+class SecretSourceResolver() {
     private val nullSubstitutor: StringSubstitutor
     private val substitutor: StringSubstitutor
 
@@ -33,6 +33,7 @@ class SecretSourceResolver {
                 "file" to FileStringLookup.INSTANCE,
                 "readFile" to FileStringLookup.INSTANCE,
                 "sysProp" to SystemPropertyLookup.INSTANCE,
+                "env" to EnvironmentVariableLookup.INSTANCE,
                 "decodeBase64" to DecodeBase64Lookup.INSTANCE,
                 "json" to JsonLookup.INSTANCE
             )
@@ -59,7 +60,7 @@ class SecretSourceResolver {
      * @return Encoded string
      * @since 1.25
      */
-    fun encode(@CheckForNull toEncode: String?): String? {
+    fun encode(toEncode: String?): String? {
         return toEncode?.replace(enclosedBy, escapeEnclosedBy)
     }
 
@@ -92,10 +93,10 @@ class SecretSourceResolver {
 
 
 
-        @Deprecated("use {@link #resolve(String)}} instead.")
-        fun resolve(context: ConfigurationContext, toInterpolate: String?): String {
-            return context.getSecretSourceResolver().resolve(toInterpolate)
-        }
+//        @Deprecated("use {@link #resolve(String)}} instead.")
+//        fun resolve(context: ConfigurationContext, toInterpolate: String?): String {
+//            return context.getSecretSourceResolver().resolve(toInterpolate)
+//        }
     }
 }
 
@@ -111,20 +112,14 @@ internal class UnresolvedLookup private constructor() : StringLookup {
     }
 }
 
-internal class ConfigurationContextStringLookup private constructor() : StringLookup {
 
-
-    override fun lookup(key: String?): String {
-        return ""
-    }
-}
 
 internal class SystemPropertyLookup : StringLookup {
     private val logger = PipelineLogger.getLogger()
-    override fun lookup(@NonNull key: String?): String {
+    override fun lookup(key: String?): String {
         val output = System.getProperty(key)
         if (output == null) {
-            logger.system("Configuration import: System Properties did not contain the specified key $key. Will default to empty string.")
+            logger.error("Configuration import: System Properties did not contain the specified key $key. Will default to empty string.")
             return ""
         }
         return output
@@ -135,9 +130,25 @@ internal class SystemPropertyLookup : StringLookup {
     }
 }
 
+internal class EnvironmentVariableLookup : StringLookup {
+    private val logger = PipelineLogger.getLogger()
+    override fun lookup(key: String?): String {
+        val output = System.getenv(key)
+        if (output == null) {
+            logger.error("Configuration import: System environment did not contain the specified key $key. Will default to empty string.")
+            return ""
+        }
+        return output
+    }
+
+    companion object {
+        val INSTANCE = EnvironmentVariableLookup()
+    }
+}
+
 internal class FileStringLookup : StringLookup {
     private val logger = PipelineLogger.getLogger()
-    override fun lookup(@NonNull key: String?): String? {
+    override fun lookup(key: String?): String? {
         return try {
             String(Files.readAllBytes(Paths.get(key)), StandardCharsets.UTF_8)
         } catch (e: IOException) {
@@ -155,7 +166,7 @@ internal class FileStringLookup : StringLookup {
 }
 
 internal class Base64Lookup : StringLookup {
-    override fun lookup(@NonNull key: String): String {
+    override fun lookup(key: String): String {
         return Base64.getEncoder().encodeToString(key.toByteArray(StandardCharsets.UTF_8))
     }
 
@@ -165,7 +176,7 @@ internal class Base64Lookup : StringLookup {
 }
 
 internal class DecodeBase64Lookup : StringLookup {
-    override fun lookup(@NonNull key: String): String {
+    override fun lookup(key: String): String {
         return String(Base64.getDecoder().decode(key.toByteArray(StandardCharsets.UTF_8)), StandardCharsets.UTF_8)
     }
 
@@ -176,7 +187,7 @@ internal class DecodeBase64Lookup : StringLookup {
 
 internal class FileBase64Lookup : StringLookup {
     private val logger = PipelineLogger.getLogger()
-    override fun lookup(@NonNull key: String?): String? {
+    override fun lookup(key: String?): String? {
         return try {
             val fileContent = Files.readAllBytes(Paths.get(key))
             Base64.getEncoder().encodeToString(fileContent)
@@ -196,16 +207,17 @@ internal class FileBase64Lookup : StringLookup {
 
 internal class JsonLookup private constructor() : StringLookup {
     private val logger = PipelineLogger.getLogger()
-    override fun lookup(@NonNull key: String): String {
-        val components = key.split(":".toRegex(), limit = 2).toTypedArray()
-        val jsonFieldName = components[0]
-        val json = components[1]
-        val root = Json.parseToJsonElement(json).jsonObject
-        val output = root[jsonFieldName]?.jsonPrimitive?.content
-
-        return output ?: "".also {
-            logger.system("Configuration import: JSON did not contain the specified key $jsonFieldName. Will default to empty string.")
-        }
+    override fun lookup(key: String): String {
+//        val components = key.split(":".toRegex(), limit = 2).toTypedArray()
+//        val jsonFieldName = components[0]
+//        val json = components[1]
+//        val root = Json.parseToJsonElement(json).jsonObject
+//        val output = root[jsonFieldName]?.jsonPrimitive?.content
+//
+//        return output ?: "".also {
+//            logger.system("Configuration import: JSON did not contain the specified key $jsonFieldName. Will default to empty string.")
+//        }
+        return ""
     }
 
     companion object {
@@ -217,7 +229,7 @@ internal class JsonLookup private constructor() : StringLookup {
 internal class FixedInterpolatorStringLookup(
     stringLookupMap: Map<String, StringLookup>,
     /** The default string lookup.  */
-    private val defaultStringLookup: StringLookup?
+    private var defaultStringLookup: StringLookup?
 ) :
     StringLookup {
     /** The map of String lookups keyed by prefix.  */
@@ -234,6 +246,7 @@ internal class FixedInterpolatorStringLookup(
         for ((key, value) in stringLookupMap) {
             this.stringLookupMap[toKey(key)] = value
         }
+        defaultStringLookup = stringLookupMap["env"]
     }
 
     /**

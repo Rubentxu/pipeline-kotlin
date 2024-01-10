1838ae2cfefa1afd4f38e30a55311e4ae6da49e2
Rubentxu <rubentxu74@gmail.com>
2024-01-10 11:59:12 +0100

    documentacion para PropertySet y algunas correcciones

--- pipeline-model/src/main/kotlin/dev/rubentxu/pipeline/model/mapper/PropertySet.kt
+++ pipeline-model/src/main/kotlin/dev/rubentxu/pipeline/model/mapper/PropertySet.kt
@@ -5,17 +5,47 @@ import arrow.core.raise.Raise
 import arrow.core.raise.either
 import arrow.core.raise.ensure
 
-
+/**
+ * Type alias for a map where the key is a string and the value can be any type.
+ * This is used to represent a set of properties, where each property is a key-value pair.
+ */
 typealias PropertySet = Map<String, Any?>
 
+/**
+ * Data class for validation errors.
+ * @property message The error message.
+ * This is used to represent an error that occurs during validation.
+ */
 data class ValidationError(val message: String)
 
+/**
+ * Exception class for validation errors.
+ * @property message The error message.
+ * This is used to throw an exception when a validation error occurs.
+ */
+class ValidationErrorException(message: String) : Exception(message)
+
+/**
+ * Extension function for String class to convert a string to a PropertyPath.
+ * @return Either a ValidationError or a PropertyPath.
+ * This is used to convert a string to a PropertyPath. If the string contains a ".", it is converted to a NestedPath.
+ * Otherwise, it is converted to a PathSegment.
+ */
 inline fun String.propertyPath(): Either<ValidationError, PropertyPath> =
     if (this.contains(".")) NestedPath(this) else PathSegment(this)
 
-
+/**
+ * Sealed interface for PropertyPath.
+ * This is used to represent a path to a property. It can be a PathSegment or a NestedPath.
+ */
 sealed interface PropertyPath
 
+/**
+ * Data class for PathSegment.
+ * @property path The path segment.
+ * This is used to represent a segment of a path to a property. It contains methods to check if the segment contains an index,
+ * to get the key from the segment, to get the index from the segment, and to check if the index is a number.
+ */
 data class PathSegment private constructor(val path: String) : PropertyPath {
     private val indexRegex = """.*\[(.*)\]""".toRegex()
 
@@ -29,9 +59,7 @@ data class PathSegment private constructor(val path: String) : PropertyPath {
     }
 
     fun containsIndex(): Boolean = indexRegex.matches(path)
-
     fun getKey(): String = path.substringBefore("[")
-
     fun getIndex(): Int? {
         try {
             return indexRegex.find(path)?.groupValues?.get(1)?.toInt()
@@ -39,20 +67,27 @@ data class PathSegment private constructor(val path: String) : PropertyPath {
             return null
         }
     }
-
     fun indexIsNumber(): Boolean = getIndex()?.let { it is Int } ?: false
-
 }
 
+/**
+ * Data class for NestedPath.
+ * @property path The nested path.
+ * This is used to represent a nested path to a property. It contains methods to validate the path and to get the path segments from the nested path.
+ */
 data class NestedPath private constructor(val path: String) : PropertyPath {
-
     companion object {
+        private val nestedPathRegex = """([a-zA-Z0-9]+(\[[0-9]+\])?\.)*[a-zA-Z0-9]+(\[[0-9]+\])?""".toRegex()
+
         operator fun invoke(path: String): Either<ValidationError, PropertyPath> = either {
+            ensure(validatePath(path)) { ValidationError("NestedPath is not valid for path '$path'") }
             ensure(path.isNotEmpty()) { ValidationError("NestedPath cannot be empty") }
             ensure(path.contains(".") || path.contains("[")) { ValidationError("NestedPath must contain '.' or '[]'") }
             ensure(path.all { it.isDefined() }) { ValidationError("NestedPath can only contain alphanumeric characters : ${this}") }
             NestedPath(path)
         }
+
+        fun validatePath(path: String): Boolean = nestedPathRegex.matches(path)
     }
 
     fun getPathSegments(): Either<NonEmptyList<ValidationError>, List<PathSegment>> = either {
@@ -62,6 +97,13 @@ data class NestedPath private constructor(val path: String) : PropertyPath {
     }
 }
 
+/**
+ * Gets the required value of type T from the PropertySet.
+ * @param path The PropertyPath.
+ * @return Either a ValidationError or the required value of type T.
+ * This is used to get a required value from the PropertySet. If the path is a PathSegment, it calls the required function for PathSegment.
+ * If the path is a NestedPath, it calls the required function for NestedPath.
+ */
 inline fun <reified T> PropertySet.required(path: PropertyPath): Either<ValidationError, T> = either {
     return when (path) {
         is PathSegment -> required<T>(path)
@@ -69,6 +111,13 @@ inline fun <reified T> PropertySet.required(path: PropertyPath): Either<Validati
     }
 }
 
+/**
+ * Gets the required value of type T from the PropertySet.
+ * @param segment The PathSegment.
+ * @return Either a ValidationError or the required value of type T.
+ * This is used to get a required value from the PropertySet using a PathSegment. It checks if the key is present in the PropertySet,
+ * if the value is not null, and if the value is of type T. If all checks pass, it returns the value. Otherwise, it returns a ValidationError.
+ */
 inline fun <reified T> PropertySet.required(segment: PathSegment): Either<ValidationError, T> = either {
     ensure(containsKey(segment.getKey())) { ValidationError("PathSegment '${segment.getKey()}' not found in PropertySet") }
     val value: Any? = getValue<T>(segment, this)
@@ -77,6 +126,15 @@ inline fun <reified T> PropertySet.required(segment: PathSegment): Either<Valida
     value
 }
 
+/**
+ * Gets the value of type T from the PropertySet.
+ * @param segment The PathSegment.
+ * @param raise The Raise instance for ValidationError.
+ * @return The value of type T.
+ * This is used to get a value from the PropertySet using a PathSegment. If the segment contains an index, it checks if the index is a number,
+ * if the value is a list, and if the index is in range of the list. If all checks pass, it returns the value at the index. Otherwise, it returns a ValidationError.
+ * If the segment does not contain an index, it returns the value.
+ */
 inline fun <reified T>  PropertySet.getValue(
     segment: PathSegment,
     raise: Raise<ValidationError>,
@@ -94,15 +152,55 @@ inline fun <reified T>  PropertySet.getValue(
     return value
 }
 
+/**
+ * Checks if the index is in range of the collection.
+ * @param index The index to check.
+ * @return Boolean indicating if the index is in range of the collection.
+ * This is used to check if an index is in range of a collection. It returns true if the index is greater than or equal to 0 and less than the size of the collection.
+ * Otherwise, it returns false.
+ */
 fun List<*>.isInRangeCollection(index: Int): Boolean {
     return index >= 0 && index < size
 }
 
-
+/**
+ * Gets the required value of type T from the PropertySet.
+ * @param path The NestedPath.
+ * @return Either a ValidationError or the required value of type T.
+ * This is used to get a required value from the PropertySet using a NestedPath. It gets the path segments from the nested path,
+ * then it folds the segments (except the last one) to get a PropertySet for each segment. Finally, it gets the required value from the last segment.
+ */
 inline fun <reified T > PropertySet.required(path: NestedPath): Either<ValidationError, T> = either {
     val keys = path.getPathSegments().getOrElse { raise(it.first()) }
     val result = keys.dropLast(1).fold(this@required) { acc: PropertySet, key: PathSegment ->
         acc.required<PropertySet>(key).bind()
     }.required<T>(keys.last()).bind()
     result
+}
+
+/**
+ * Gets the required value of type T from the PropertySet.
+ * @param path The Either instance of ValidationError or PropertyPath.
+ * @return Either a ValidationError or the required value of type T.
+ * This is used to get a required value from the PropertySet using an Either instance of ValidationError or PropertyPath.
+ * If the path is a ValidationError, it returns the ValidationError. If the path is a PropertyPath, it gets the required value from the PropertyPath.
+ */
+inline fun <reified T> PropertySet.required(path: Either<ValidationError, PropertyPath>): Either<ValidationError, T> {
+    return path.flatMap { validPath ->
+        this.required<T>(validPath)
+    }
+}
+
+/**
+ * Unwraps the Either instance of ValidationError or T.
+ * @return The value of type T.
+ * @throws ValidationErrorException if the Either instance is Left.
+ * This is used to unwrap an Either instance of ValidationError or T. If the Either instance is a ValidationError, it throws a ValidationErrorException.
+ * If the Either instance is a T, it returns the T.
+ */
+fun <T> Either<ValidationError, T>.unwrap(): T {
+    return this.fold(
+        { throw ValidationErrorException(it.message) },
+        { it }
+    )
 }
--- pipeline-model/src/test/kotlin/dev/rubentxu/pipeline/model/mapper/PropertySetTest.kt
+++ pipeline-model/src/test/kotlin/dev/rubentxu/pipeline/model/mapper/PropertySetTest.kt
@@ -6,96 +6,138 @@ import io.kotest.matchers.shouldBe
 
 class PropertySetTest : StringSpec({
 
+
     "required should return value if present" {
         val propertySet: PropertySet = mutableMapOf("key" to "value")
-        val pathSegment = "key".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key".propertyPath()
+        val result = propertySet.required<String>(pathSegment).unwrap()
 
-        val result = propertySet.required<String>(pathSegment)
-        result shouldBe "value".right()
+        result shouldBe "value"
     }
 
     "required should fail if key is not present" {
         val propertySet: PropertySet = mutableMapOf("key" to "value")
-        val pathSegment = "key2".propertyPath().getOrElse { throw Exception(it.message) }
-        val result = propertySet.required<String>(pathSegment)
+        val result = "key2".propertyPath().flatMap { pathSegment ->
+             propertySet.required<String>(pathSegment)
+        }
         result shouldBe ValidationError("PathSegment 'key2' not found in PropertySet").left()
     }
 
     "required should fail if value is null" {
         val propertySet: PropertySet = mutableMapOf("key" to null)
-        val pathSegment = "key".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key".propertyPath()
         val result = propertySet.required<String>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key' is null in PropertySet").left()
     }
 
     "required should fail if value is not of type T" {
         val propertySet: PropertySet = mutableMapOf("key" to 123)
-        val pathSegment = "key".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key".propertyPath()
         val result = propertySet.required<String>(pathSegment)
         result shouldBe ValidationError("Value for PathSegment 'key' is not of type class kotlin.String").left()
     }
 
     "required should return value if value is a list and index is present" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[0]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[0]".propertyPath()
         val result = propertySet.required<String>(pathSegment)
         result shouldBe "value1".right()
     }
 
     "required should fail if value is a list and index is not present" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key".propertyPath()
         val result = propertySet.required<String>(pathSegment)
         result shouldBe ValidationError("Value for PathSegment 'key' is not of type class kotlin.String").left()
     }
 
     "required should fail if value is a list and index is out of range" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[2]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[2]".propertyPath()
         val result = propertySet.required<String>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key[2]' index 2 is out of range").left()
     }
 
     "required should fail if value is a list and index is not a number" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[a]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[a]".propertyPath()
         val result = propertySet.required<List<String>>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key[a]' does not contain a number index").left()
     }
 
     "required should fail if value is a list and index is not range" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[-1]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[-1]".propertyPath()
         val result = propertySet.required<List<String>>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key[-1]' index -1 is out of range").left()
     }
 
     "required should fail if value is a list and index is not present and value is a list" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key".propertyPath()
         val result = propertySet.required<String>(pathSegment)
         result shouldBe ValidationError("Value for PathSegment 'key' is not of type class kotlin.String").left()
     }
 
     "required should fail if value is a list and index is out of range and value is a list" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[2]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[2]".propertyPath()
         val result = propertySet.required<List<String>>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key[2]' index 2 is out of range").left()
     }
 
     "required should fail if value is a list and index is not a number and value is a list" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[a]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[a]".propertyPath()
         val result = propertySet.required<List<String>>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key[a]' does not contain a number index").left()
     }
 
     "required should fail if value is a list and index is not range and value is a list" {
         val propertySet: PropertySet = mutableMapOf("key" to listOf("value1", "value2"))
-        val pathSegment = "key[-1]".propertyPath().getOrElse { throw Exception(it.message) }
+        val pathSegment = "key[-1]".propertyPath()
         val result = propertySet.required<List<String>>(pathSegment)
         result shouldBe ValidationError("PathSegment 'key[-1]' index -1 is out of range").left()
+    }
+
+    "required should return value if nested path is present" {
+        val propertySet: PropertySet = mutableMapOf("key1" to mutableMapOf("key2" to listOf("value1", "value2")))
+        val nestedPath = "key1.key2[0]".propertyPath()
+        val result = propertySet.required<String>(nestedPath).unwrap()
+
+        result shouldBe "value1"
+    }
+
+    "required should fail if nested path is not present" {
+        val propertySet: PropertySet = mutableMapOf("key1" to mutableMapOf("key2" to listOf("value1", "value2")))
+        val nestedPath = "key1.key3[0]".propertyPath()
+        val result = propertySet.required<String>(nestedPath)
+
+        result shouldBe ValidationError("PathSegment 'key3' not found in PropertySet").left()
+    }
+
+    "required should fail if nested path is invalid" {
+        val propertySet: PropertySet = mutableMapOf("key1" to mutableMapOf("key2" to listOf("value1", "value2")))
+        val nestedPath = "key1..key2[0]".propertyPath()
+        val result = propertySet.required<String>(nestedPath)
+
+        result shouldBe ValidationError("NestedPath is not valid for path 'key1..key2[0]'").left()
+    }
+
+    "required should fail if nested path index is out of range" {
+        val propertySet: PropertySet = mutableMapOf("key1" to mutableMapOf("key2" to listOf("value1", "value2")))
+        val nestedPath = "key1.key2[2]".propertyPath()
+        val result = propertySet.required<String>(nestedPath)
+
+        result shouldBe ValidationError("PathSegment 'key2[2]' index 2 is out of range").left()
+    }
+
+    "required should fail if nested path index is not a number" {
+        val propertySet: PropertySet = mutableMapOf("key1" to mutableMapOf("key2" to listOf("value1", "value2")))
+        val nestedPath = "key1.key2[a]".propertyPath()
+        val result = propertySet.required<String>(nestedPath)
+
+        result shouldBe ValidationError("NestedPath is not valid for path 'key1.key2[a]'").left()
     }
 
 })
